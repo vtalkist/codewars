89bf1c298c836db3b98dda3e06a903f07c3aa0d1
vtalkist <57374114+vtalkist@users.noreply.github.com>
2020-01-25 21:59:37 +0200

    Create validate_ip_address.c
    This finction is checking an ip address.

---
+++ validate_ip_address.c
@@ -0,0 +1,15 @@
+int is_valid_ip(const char * addr) 
+{
+	char* copy = strdup(addr), delim[2] = ".", *istr = strtok(copy, delim);
+  int i = 0;
+  while(istr != 0)
+	{
+		if (atoi(istr) > 255 || atoi(istr) < 0 || atoi(istr) == 0 && *istr != '0' || *istr == '0' || strchr(istr, ' ') != 0)
+			return 0;
+		istr = strtok(0, delim);
+    i++;
+  }
+  if(i != 4)
+    return 0;
+	return 1;
+}
